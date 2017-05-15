9bd19b890359ca75b898ff32932248bf15b9f9de
Liu Junying <2014213172@bupt.edu.cn>
2017-05-15 19:33:14 +0800

    test

--- src/Model/ShowTable.java
+++ src/Model/ShowTable.java
@@ -47,7 +47,14 @@ public class ShowTable {
      *
      * @param filePath
      */
-    private void initFilms(String filePath) {
+    public static ArrayList<Film> initFilms(String filePath) {
+    	ArrayList<Film> movie = new ArrayList<Film>();
+    	movie = Film.readMovie(filePath);
+    	
+    	for (int i = 0; movie.get(i)!= null;i++){
+    		Film.readTime(movie.get(i));         	
+        }
+    	return movie;
     }
 
     /**
